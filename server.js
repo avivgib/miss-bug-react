21dcbb36b6eaf48952ad9a55c0d72b56557a70f4
Aviv Gibli <avivgib1012@gmail.com>
2025-04-02 01:06:00 +0300

    Implement authentication and user management: add signup, login, and logout endpoints; enforce authorization for bug operations; enhance user data handling

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cookieParser from 'cookie-parser'
 
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
+import { authService } from './services/auth.service.js'
+import { userService } from './services/user.service.js'
 
 const app = express()
 
@@ -20,7 +22,7 @@ app.get('/api/bug/bugs-pdf', (req, res) => {
 // Fetch all bugs
 app.get('/api/bug', (req, res) => {
     bugService.query(req.query)
-        .then(({bugs, total}) => {
+        .then(({ bugs, total }) => {
             loggerService.info(`Fetched ${bugs.length} bugs, total: ${total}`)
             res.status(200).send({ bugs, total })
         })
@@ -32,8 +34,18 @@ app.get('/api/bug', (req, res) => {
 
 // Create New Bug
 app.post('/api/bug', (req, res) => {
-    const bugToSave = req.body
-    console.log('bugToSave', bugToSave)
+    const authenticatedUser = authService.validateToken(req.cookies.loginToken)
+    if (!authenticatedUser) return res.status(401).send('Unauthorized: Please log in to add a bug')
+
+    const bugToSave = {
+        ...req.body,
+        createdAt: Date.now(),
+        creator: {
+            _id: authenticatedUser._id,
+            fullname: authenticatedUser.fullname
+        }
+    }
+    // console.log('bugToSave', bugToSave)
 
     bugService.save(bugToSave)
         .then(bug => {
@@ -48,16 +60,31 @@ app.post('/api/bug', (req, res) => {
 
 // Update Bug
 app.put('/api/bug/:bugId', (req, res) => {
-    const bugToSave = req.body
+    const { bugId } = req.params
+    const authenticatedUser = authService.validateToken(req.cookies.loginToken)
+    if (!authenticatedUser) return res.status(401).send('Unauthorized: Please log in to update the bug')
 
-    bugService.save(bugToSave)
+    bugService.getById(bugId)
         .then(bug => {
-            loggerService.info(`Bug update with ID - ${bug._id}`)
-            res.send(bug)
+            if (bug.creator._id !== authenticatedUser._id) {
+                return res.status(403).send('Not authorized to update this bug')
+            }
+
+            const bugToUpdate = { ...bug, ...req.body }
+
+            bugService.save(bugToUpdate)
+                .then(updatedBug => {
+                    loggerService.info(`Bug update with ID - ${updatedBug._id}`)
+                    res.send(updatedBug)
+                })
+                .catch(err => {
+                    loggerService.error('Failed to update bug', err)
+                    res.status(500).send('Cannot update bug')
+                })
         })
         .catch(err => {
-            loggerService.error('Failed to update bug', err)
-            res.status(500).send('Cannot update bug')
+            loggerService.error('Bug not found', err)
+            res.status(404).send('Bug not found')
         })
 })
 
@@ -87,16 +114,102 @@ app.get('/api/bug/:bugId', (req, res) => {
 //* Remove
 app.delete('/api/bug/:bugId', (req, res) => {
     const { bugId } = req.params
-    bugService.remove(bugId)
-        .then(() => {
-            loggerService.info(`Bug removed with ID - ${bugId}`)
-            res.send('bug Removed')
+    const authenticatedUser = authService.validateToken(req.cookies.loginToken)
+    if (!authenticatedUser) return res.status(401).send('Unauthorized: Please log in to delete the bug')
+
+    console.log('authenticatedUser', authenticatedUser)
+
+    bugService.getById(bugId)
+        .then(bug => {
+            if (bug.creator._id !== authenticatedUser._id) {
+                return res.status(403).send('Not authorized to delete this bug')
+            }
+
+            bugService.remove(bugId)
+                .then(() => {
+                    loggerService.info(`Bug removed with ID - ${bugId}`)
+                    res.send('Bug Removed')
+                })
+                .catch(err => {
+                    loggerService.error('Failed to remove bug', err)
+                    res.status(500).send('Cannot remove bug')
+                })
         })
         .catch(err => {
-            loggerService.error('Failed to remove bug', err)
-            res.status(500).send('Cannot remove bug')
+            loggerService.error('Bug not found', err)
+            res.status(500).send('Bug not found')
         })
 })
+
+// Auth API
+app.post('/api/auth/signup', (req, res) => {
+    const credentials = req.body
+
+    userService.add(credentials)
+        .then(user => {
+            if (user) {
+                const loginToken = authService.getLoginToken(user)
+                res.cookie('loginToken', loginToken)
+                res.send(user)
+            } else {
+                res.status(400).send('Cannot signup')
+            }
+        })
+        .catch(err => res.status(404).send(`Username Taken - ${err}`))
+})
+
+app.post('/api/auth/login', (req, res) => {
+    const credentials = req.body
+    // console.log(`credentials: ${JSON.stringify(credentials)}`)
+
+    authService.checkLogin(credentials)
+        .then(user => {
+            const loginToken = authService.getLoginToken(user)
+            res.cookie('loginToken', loginToken)
+            res.send(user)
+        })
+        .catch(() => res.status(404).send('Invalid Credentials'))
+})
+
+app.get('/api/auth/logout', (req, res) => {
+    res.clearCookie('loginToken')
+    res.send('logged-out')
+})
+
+
+// Admin Options
+// Fetch all users
+app.get('/api/user', (req, res) => {
+    userService.query()
+        .then(users => res.send(users))
+        .catch(err => {
+            loggerService.error('Failed to fetch users', err)
+            res.status(500).send('Cannot fetch users')
+        })
+})
+
+// Get user by ID
+app.get('/api/user/:userId', (req, res) => {
+    const { userId } = req.params
+    userService.getById(userId)
+        .then(user => res.send(user))
+        .catch(err => {
+            loggerService.error('Failed to fetch user', err)
+            res.status(500).send('Cannot fetch user')
+        })
+})
+
+// Delete user
+app.delete('/api/user/:userId', (req, res) => {
+    const { userId } = req.params
+    userService.remove(userId)
+        .then(() => res.send({ msg: 'User deleted' }))
+        .catch(err => {
+            loggerService.error('Failed to delete user', err)
+            res.status(500).send('Cannot delete user')
+        })
+})
+
 
 const port = 3030
 app.listen(port, () =>
