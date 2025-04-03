820a47a0e027093b4388b3f2a13ad52dbf858ad5
Aviv Gibli <avivgib1012@gmail.com>
2025-04-03 16:05:01 +0300

    Refactor AboutUs component: enhance styling and structure for improved layout and readability

--- public/pages/AboutUs.jsx
+++ public/pages/AboutUs.jsx
@@ -1,7 +1,16 @@
 export function AboutUs() {
     return <section className="about-us">
         <h2>About Us</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aperiam quo veniam velit dolor reprehenderit, laudantium consequatur neque numquam labore quae. Accusamus libero perferendis ducimus? Alias unde hic quisquam doloremque.</p>
-        <img src="assets/img/miss-bug.png" alt="" />
+        <span className="content">
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Magni aperiam quo veniam velit dolor reprehenderit,
+                laudantium consequatur neque numquam labore quae.
+                Accusamus libero perferendis ducimus?
+                Alias unde hic quisquam doloremque.
+            </p>
+
+            <img src="assets/img/miss-bug.png" alt="" />
+        </span>
     </section>
 }
