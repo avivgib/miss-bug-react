8bd510461600ad80eabaf81bfd90d0ed9eef2a70
Aviv Gibli <avivgib1012@gmail.com>
2025-04-03 16:03:37 +0300

    Add authentication route and manage logged-in user state in RootCmp

--- public/RootCmp.jsx
+++ public/RootCmp.jsx
@@ -1,5 +1,6 @@
 const Router = ReactRouterDOM.HashRouter
 const { Route, Routes } = ReactRouterDOM
+const { useState } = React
 
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { AppHeader } from './cmps/AppHeader.jsx'
@@ -8,8 +9,12 @@ import { Home } from './pages/Home.jsx'
 import { BugIndex } from './pages/BugIndex.jsx'
 import { BugDetails } from './pages/BugDetails.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
+import { authService } from './services/auth.service.js'
+import { LoginSignup } from './cmps/LoginSignup.jsx'
 
 export function App() {
+    const [loggedInUser, setLoggedInUser] = useState(authService.getLoggedInUser)
+
     return <Router>
         <div className="app-wrapper">
             <UserMsg />
@@ -20,6 +25,7 @@ export function App() {
                     <Route path="/bug" element={<BugIndex />} />
                     <Route path="/bug/:bugId" element={<BugDetails />} />
                     <Route path="/about" element={<AboutUs />} />
+                    <Route path="/auth" element={<LoginSignup setLoggedInUser={setLoggedInUser} />} />
                 </Routes>
             </main>
             <AppFooter />
